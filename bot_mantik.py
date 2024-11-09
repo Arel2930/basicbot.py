08ef2feca745e2959eaaae58ff3cc0b858d617e2
Arel2930 <115020427+Arel2930@users.noreply.github.com>
2024-11-09 20:38:11 +0300

    Update bot_mantik.py

--- bot_mantik.py
+++ bot_mantik.py
@@ -1,20 +1,35 @@
-import random
+import discord
+# Discord botları için komut tabanlı bir framework sağlar. 
+# Bu framework sayesinde, botumuzun belirli komutlara yanıt vermesini kolayca tanımlayabiliriz.
+from discord.ext import commands
+from bot_mantik import gen_pass
 
-def gen_pass(pass_length):
-    
-    elements = "+-/*!&$#?=@<>"
-    password = ""
-    for i in range(pass_length):
-        password += random.choice(elements)
-    return password
+intents = discord.Intents.default()
+intents.message_content = True # botun mesaj içeriğine erişimini aktif hale getiriyoruz.
 
-def emoji_olusturucu():
-    emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
-    return random.choice(emoji)
+bot = commands.Bot(command_prefix='$', intents=intents)
+#Bu özellik, botun kendisine gönderilen komutları tanıması için bir ön ek tanımlar.
+#  $ işareti komut ön eki olarak belirlenmiştir. Yani bot sadece $ ile başlayan komutlara yanıt verir.
 
-def yazi_tura():
-    para = random.randint(0, 2)
-    if para == 0:
-        return "YAZI"
-    else:
-        return "TURA"
+@bot.event # bot belirli bir olay gerçekleştiğinde tetiklensin.
+async def on_ready(): # bot başarılı bir şekilde Discord'a bağlandığında tetiklenir
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command() # botun bir komutu tanıması için bu dekoratörü kullanırız.
+async def hello(ctx): # hello adında bir komut tanımladık. ctx(context), komutun çağrıldığı yer hakkındaki bilgileri içerir.
+    await ctx.send(f'Merhaba {bot.user}! Ben bir botum!')
+#Bu komutun çalışması için, kullanıcı sohbette $hello yazmalıdır. 
+
+@bot.command() # botun bir komutu tanıması için bu dekoratörü kullanırız.
+async def pasw(ctx): # pasw adında bir komut tanımladık. ctx(context), komutun çağrıldığı yer hakkındaki bilgileri içer
+    await ctx.send(gen_pass(10))
+#Bu komutun çalışması için kullanıcı $pasw yazmalıdır.
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+ 
+
+
+bot.run("token")
